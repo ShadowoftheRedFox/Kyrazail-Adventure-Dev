e437c3ac0641db6c61c4d5961fcbac87ea96d767
ShadowoftheRedFox <74469156+ShadowoftheRedFox@users.noreply.github.com>
2022-05-29 14:11:15 +0200

    Update lang.translate.js

--- src/www/src/js/lang/lang.translate.js
+++ src/www/src/js/lang/lang.translate.js
@@ -1,13 +1,18 @@
 /**
  * Built in translate switcher.
+ * @param {"fr" | "en"} lang language to translate
  * @param {string} str the default string to translate 
- * @param {array} [param=[]] 
+ * @param {array} [param=[]] the paramters of the string to translate, if there is.
  */
-function translate(str, param) {
-    switch (str) {
-        case "a":
-            return "a";
-        default:
-            return "undefined";
+function translate(lang, str, ...param) {
+    if (lang === "fr") {
+        switch (str) {
+            case "a":
+                return "a";
+            default:
+                return "undefined";
+        }
+    } else {
+        //if lang is unknown, translate in english
     }
 }
