928693cc59e0c4a24f86cb2c855d06d82b9d7ad5
ShadowoftheRedFox <leon.merin@gmail.com>
2022-09-03 11:26:12 +0200

    added comment to know what's going on

--- App/src/src.main.js
+++ App/src/src.main.js
@@ -1,24 +1,14 @@
 /// <reference path="../ts/type.d.ts"/>
 console.time("Started game in");
 
-console.log("%cKyrazail Adventure", `
-font-family: serif; 
-font-size: 32px; 
-color: black; 
-text-align: center;
-font-weight: 700; 
-background: linear-gradient(0.40turn, #F3C126, #6FE0E1, #3C1EEE);
-padding: 40px;
-border-radius: 40px;
-text-shadow: 10px 5px 5px rgba(0, 0, 0, 0.344);
-border: 2px ivory solid;
-`);
-document.ondrag = document.ondragstart = document.oncontextmenu = function () { return false; };
+console.log("%cKyrazail Adventure", `font-family: serif; font-size: 32px; color: black; text-align: center;font-weight: 700; background: linear-gradient(0.40turn, #F3C126, #6FE0E1, #3C1EEE);padding: 40px;border-radius: 40px;text-shadow: 10px 5px 5px rgba(0, 0, 0, 0.344);border: 2px ivory solid;`);
+document.ondrag = document.ondragstart = document.onselectstart = document.oncontextmenu = function () { return false; };
 
 // declare all needed global variables here
 var GameAudiosToLoad = [];
 var GameImagesToLoad = [];
 var GameGlobalEvent;
+// load all script
 window.onload = ScriptLoaderManager.setup(StackLoadPlugin, 0, () => {
     ScriptLoaderManager.setup([].concat(
         StackLoadClass, StackLoadConfig,
@@ -28,14 +18,18 @@ window.onload = ScriptLoaderManager.setup(StackLoadPlugin, 0, () => {
         StackLoadManager, StackLoadUtil,
         StackLoadLanguage, StackLoadBuild
     ), 0, () => {
+        // load all sounds
         DataLoaderManager.setup(StackLoadData, 0, () => {
             WindowManager.init();
             try {
+                // launch manager
                 GameGlobalEvent = new EventEmitter();
                 LoadingScreenManager.init();
                 KeyboardTrackerManager.init();
                 MouseTrackerManager.init();
+                // set a random title
                 document.getElementById("title").innerText += `: ${ConfigConst.TITLE.random()}`;
+                // start the game
                 window.game = new Game();
             } catch (e) {
                 WindowManager.fatal(e);
