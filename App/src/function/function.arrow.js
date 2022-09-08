25d50644d74de55cd89658fb706ba42927402dbc
ShadowoftheRedFox <leon.merin@gmail.com>
2022-09-08 18:32:32 +0200

    Update function.arrow.js
    added hat arrow, and removed custom size (since we return an image, we can just choose the size with the drawImage method)
    also added form method to prevent useless copy paste

--- App/src/function/function.arrow.js
+++ App/src/function/function.arrow.js
@@ -28,14 +28,12 @@ ArrowDrawer.createStrictContext = function (w, h) {
 
 /**
  * @param {HTMLCanvasElement} img the type of arrow to draw
- * @param {number} w width
- * @param {number} h height
  * @param {"up" | "down" | "right" | "left"} a direction the arrow is pointing
  * @param {(string | CanvasGradient | CanvasPattern) | "black"} c the color of the arrow
  * @return {HTMLImageElement} the arrow image
  */
-ArrowDrawer.draw = function (img, w = 20, h = 20, a = "right", c = "black") {
-    const canvas = ArrowDrawer.createContext(w, h),
+ArrowDrawer.draw = function (img, a = "right", c = "black") {
+    const canvas = ArrowDrawer.createContext(img.width, img.height),
         ctx = canvas.getContext("2d");
     var rad = Math.PI;
     switch (a) {
@@ -52,7 +50,7 @@ ArrowDrawer.draw = function (img, w = 20, h = 20, a = "right", c = "black") {
 
     ctx.imageSmoothingQuality = "high";
     ctx.save();
-    ctx.translate(w / 2, h / 2);
+    ctx.translate(img.width / 2, img.height / 2);
     ctx.rotate(rad);
     ctx.fillStyle = c;
     // center of a rect is -w/2 and -h/2
@@ -63,17 +61,33 @@ ArrowDrawer.draw = function (img, w = 20, h = 20, a = "right", c = "black") {
 };
 
 /**
+ * Draw the arrow form.
+ * @param {(string | CanvasGradient | CanvasPattern) | "black"} c the color of the arrow
+ * @param {number[][]} arrow The array of the arrow form
+ * @returns {HTMLCanvasElement} the arrow
+ */
+ArrowDrawer.form = function (c, arrow) {
+    const canvas = ArrowDrawer.createStrictContext(arrow[0].length, arrow.length),
+        ctx = canvas.getContext("2d");
+
+    ctx.clearRect(0, 0, arrow[0].length, arrow.length);
+    ctx.fillStyle = c;
+    arrow.forEach((l, lx) => {
+        l.forEach((r, rx) => {
+            if (!!r) ctx.fillRect(rx, lx, 1, 1);
+        });
+    });
+
+    return canvas;
+};
+
+/**
  * Draw a pixelised arrow.
- * @param {number} w width
- * @param {number} h height
  * @param {"up" | "down" | "right" | "left"} a direction the arrow is pointing
  * @param {(string | CanvasGradient | CanvasPattern) | "black"} c the color of the arrow
  * @returns {HTMLCanvasElement} the arrow
  */
-ArrowDrawer.pixel = function (w = 20, h = 20, a = "right", c = "black") {
-    const canvas = ArrowDrawer.createStrictContext(20, 20),
-        ctx = canvas.getContext("2d");
-
+ArrowDrawer.pixel = function (a = "right", c = "black") {
     //draw the arrow form
     const arrow = [
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -95,16 +109,45 @@ ArrowDrawer.pixel = function (w = 20, h = 20, a = "right", c = "black") {
         [0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0],
         [0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0],
         [0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0]
     ];
 
-    ctx.clearRect(0, 0, 20, 20);
-    ctx.fillStyle = c;
-    arrow.forEach((l, lx) => {
-        l.forEach((r, rx) => {
-            if (r == 1) ctx.fillRect(rx, lx, 1, 1);
-        });
-    });
+    return ArrowDrawer.draw(ArrowDrawer.form(c, arrow), a, c);
+};
 
-    return ArrowDrawer.draw(canvas, w, h, a);
+/**
+ * Draw a arrow hat.
+ * @param {"up" | "down" | "right" | "left"} a direction the arrow is pointing
+ * @param {(string | CanvasGradient | CanvasPattern) | "black"} c the color of the arrow
+ * @returns {HTMLCanvasElement} the arrow
+ */
+ArrowDrawer.hat = function (a = "right", c = "black") {
+    //draw the arrow form
+    const arrow = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+        [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+        [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1],
+        [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+        [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+    ];
+
+    return ArrowDrawer.draw(ArrowDrawer.form(c, arrow), a, c);
 };
