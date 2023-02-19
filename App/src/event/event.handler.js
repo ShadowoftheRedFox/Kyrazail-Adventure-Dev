48b825ce099b647589de7f002c0b5a0d5ba61ca4
ShadowoftheRedFox <leon.merin@gmail.com>
2023-02-19 16:21:10 +0100

    event system update

--- App/src/event/event.handler.js
+++ App/src/event/event.handler.js
@@ -1,175 +1,72 @@
-/* Event reminders
-type:
-    1: something to display in the dialog interface
-    2: string input
-    3: choice between multiple choices
-*/
-
-const TestEvents = {
-    StartNewGame: {
-        type: 1,
-        list: [{
-            text: "You are a new soul that just felt from the soul tree. ",
-            name: "Unknown Goddess",
-            font: ["Unreadable", "Azure"],
-            image: null,
-            side: 0,
-            next: false,
-            skipable: true,
-            textSpeed: 5,
-            stop: false,
-            fullscreen: false,
-            announcement: false
-        }, {
-            text: "You will get a new existence in this world. ",
-            name: "Unknown Goddess",
-            font: ["Unreadable", "Azure"],
-            image: null,
-            side: 0,
-            next: false,
-            skipable: true,
-            textSpeed: 5,
-            stop: true,
-            fullscreen: false,
-            announcement: false
-        }, {
-            text: "\n \nChoose your name carefully, young one.",
-            name: "Unknown Goddess",
-            font: ["Unreadable", "Azure"],
-            image: null,
-            side: 0,
-            next: true,
-            skipable: true,
-            textSpeed: 5,
-            stop: true,
-            fullscreen: false,
-            announcement: false
-        }],
-        end: (...args) => { GameEvent.emit("MainCharacterName"); },
-        stop: ["main"],
-        start: ["dialogue"],
-        init: (...args) => { GameGlobalObject.newGame(); }
-    },
-    MainCharacterName: {
-        type: 2,
-        list: [{
-            text: "Your first name is:",
-            param: []
-        }, {
-            text: "Your last name is:",
-            param: []
-        }],
-        end: (...args) => {
-            window.game.global.player.firstName = args[0];
-            window.game.global.player.lastName = args[1];
-            GameEvent.emit("MainCharacterSpeacies");
-
-            window.game.state.menu.map.activated = true;
-            window.game.state.menu.map.changeMap(window.game, "Map001");
-        },
-        stop: [],
-        start: ["dialogue"],
-        init: () => { }
-    },
-    MainCharacterSpeacies: {
-        type: 3,
-        list: [{
-            //first item is always the question
-            text: "You're not a soul anymore, but %1 %2. But you still need a body. Choose your species %1:",
-            param: [["global", "player", "firstName"], ["global", "player", "lastName"]]
-        }, {
-            text: "Elf",
-            param: []
-        }, {
-            text: "Dwarf",
-            param: []
-        }, {
-            text: "Demon",
-            param: []
-        }, {
-            text: "Human",
-            param: []
-        }, {
-            text: "Vampire",
-            param: []
-        }, {
-            text: "Angel",
-            param: []
-        }, {
-            text: "Fairy",
-            param: []
-        }],
-        end: (...args) => {
-            window.game.global.player.species = args[0].toLowerCase();
-            GameEvent.emit("");
-        },
-        stop: [],
-        start: ["dialogue"],
-        init: () => { }
-    }
-};
-
 function GameEventHandler() {
     throw new Error("This is a static class.");
 }
 
-/**
- * @param {GameEvents} event 
- */
-GameEventHandler.handle = function (event) {
-    const scope = window.game,
-        events = scope.cache.data.event;
+/**@type {GameScope} */
+GameEventHandler.scope = null;
 
-
-    // stop all given interface
-    if (event.stop.length > 0) event.stop.forEach(s => { scope.state.menu[s].activated = false; });
-    // start all given interface
-    if (event.start.length > 0) event.start.forEach(s => { scope.state.menu[s].activated = true; });
-
-    if (event.init && typeof event.init == "function") {
-        event.init();
-    }
-
-    switch (event.type) {
-        case 1:
-            scope.state.menu.dialogue.textList = event.list;
-            scope.state.menu.dialogue.callback = event.end;
-            scope.state.menu.dialogue.eventType = event.type;
-            break;
-        case 2:
-            scope.state.menu.dialogue.inputList = event.list;
-            scope.state.menu.dialogue.callback = event.end;
-            scope.state.menu.dialogue.eventType = event.type;
-            break;
-        case 3:
-            scope.state.menu.dialogue.inputList = event.list;
-            scope.state.menu.dialogue.callback = event.end;
-            scope.state.menu.dialogue.eventType = event.type;
-            break;
-        default:
-            WindowManager.fatal(new ReferenceError(`${event.type} is an inkown event type.`));
-            break;
-    }
-
+GameEventHandler.init = function (scope) {
+    GameEventHandler.scope = scope;
 };
 
-GameEvent.on("StartNewGame", () => {
-    const scope = window.game,
-        events = scope.cache.data.event;
+/**
+ * 
+ * @param {GameEventCodeName} EventName 
+ * @param  {...any} args 
+ */
+GameEventHandler.handle = function (EventName, ...args) {
+    const scope = GameEventHandler.scope;
+    const data = scope.cache.data.event;
+    if (!args) args = [];
+    if (!data[EventName]) {
+        console.warn(`Got an unknown event code: ${EventName}\nWith args: \n${args.join("\n")}`);
+    }
 
-    //TODO save the game in an auto save if a game is currently being played
+    switch (EventName) {
+        default:
+            GameEventHandler.run(data[EventName]);
+            break;
+        case "Test":
+            console.log(`Got a test event, with args: \n${args.join("\n")}`);
+            break;
+        case "NewGame":
+            // TODO create a save file
+            // TODO start reminder
+            GameEventHandler.run(data[EventName]);
+            break;
+    }
+};
 
-    //TODO launch a new game
-
-    //TODO also create on start 2 auto save files
-
-    GameEventHandler.handle(TestEvents.StartNewGame);
-});
-
-
-GameEvent.on("MainCharacterName", () => {
-    const scope = window.game,
-        events = scope.cache.data.event;
-
-    GameEventHandler.handle(TestEvents.MainCharacterName);
-});
+/**
+ * 
+ * @param {GameEvent} event 
+ */
+GameEventHandler.run = function (event) {
+    // console.log(event);
+    // stop the wanted interfaces
+    event.stop.forEach(interface => {
+        if (this.scope.state.menu[interface]) {
+            this.scope.state.menu[interface].activated = false;
+            this.scope.state.menu[interface].needsUpdate = false;
+        }
+    })
+    // start the wanted interfaces
+    event.start.forEach(interface => {
+        if (this.scope.state.menu[interface]) {
+            this.scope.state.menu[interface].activated = true;
+            this.scope.state.menu[interface].needsUpdate = true;
+        } else if (this.scope.state.entities[interface]) {
+            this.scope.state.entities[interface].activated = true;
+            this.scope.state.entities[interface].needsUpdate = true;
+        }
+    })
+    if (["text", "announcement", "dialogue", "keyboard"].includes(event.data.type)) {
+        this.scope.state.menu.dialogue.currentEvent = event;
+    } else if (event.data.type === "map") {
+        this.scope.state.menu.map.changeMap(this.scope, event.data.map);
+    } else if (event.data.type === "timeout") {
+        setTimeout(() => {
+            GameEventHandler.handle(event.callbackEvent);
+        }, event.data.time * 1000);
+    }
+};
